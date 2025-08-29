199f1e6fb1ffefbe6519d0e804b87e1f912eb9df
Carlos <carlos_pedro17@live.com>
2025-08-29 04:40:42 -0300

    add arquivos html

--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
-from django.views import View
+from django.views import generic
+
+from blog.models.post import Post
 
 
 
-class PostView(View):
+class PostView(generic.ListView):
+    queryset= Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    context_object_name = 'post_list'  # garante que o template receba post_list
 
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello World')
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'    
